3c0729d402350a7ef4195470f629e8ab3022a82b
y0413 <1530448482@qq.com>
2020-08-22 15:24:46 +0800

    上传

---
+++ src/main/java/com/aaa/YfmsStart.java
@@ -0,0 +1,11 @@
+package com.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YfmsStart {
+    public static void main(String[] args) {
+        SpringApplication.run(YfmsStart.class,args);
+    }
+}
