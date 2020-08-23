1aaa741aa4ebf91004a642726627bb53d68057fd
y0413 <1530448482@qq.com>
2020-08-23 20:37:30 +0800

    实体类

--- src/main/java/com/test/t1.java
+++ src/main/java/com/test/t1.java
@@ -1,7 +1,6 @@
 package com.test;
 
-import com.Dao.boardDao;
-import com.Entity.board;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +8,5 @@ import javax.annotation.Resource;
 import java.util.List;
 @RestController
 public class t1 {
-        @Resource
-         boardDao boardDao;
-        @RequestMapping("select")
-        public List<board> select(){
-            System.out.println(111);
-            return  boardDao.selectAll();
-        }
+
 }
