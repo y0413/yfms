6440eb0d4884c9a6a72d0c940368d0b2f91d10c8
chy <3244305897@example.com>
2020-08-24 15:59:48 +0800

    登录

--- src/main/java/com/controller/AdminsController.java
+++ src/main/java/com/controller/AdminsController.java
@@ -1,23 +1,35 @@
 package com.controller;
 
 import com.dao.AdminsDao;
-import com.entity.Account;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.entity.Admins;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 @CrossOrigin
-@RestController
+@Controller
 @RequestMapping("AdminsController")
 public class AdminsController {
     @Resource
     AdminsDao adminsDao;
     @RequestMapping("queryName")
-    public List<Account> queryName(String account,String apwd){
+    @ResponseBody
+    public List<Admins> queryName(String account,String apwd){
         System.out.println(account);
         System.out.println(apwd);
         return adminsDao.queryName(account,apwd);
+    }
+    @RequestMapping("queryaccount")
+    @ResponseBody
+    public List<Admins> queryaccount(String account){
+        System.out.println(account);
+        return adminsDao.queryaccount(account);
+    }
+    @RequestMapping("/query")
+    @ResponseBody
+    public String query(){
+        System.out.println("xxx");
+        return "od";
     }
 }
--- src/main/java/com/dao/AdminsDao.java
+++ src/main/java/com/dao/AdminsDao.java
@@ -2,6 +2,7 @@ package com.dao;
 
 import com.entity.Account;
 import com.entity.Admins;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
@@ -11,5 +12,12 @@ import java.util.List;
 public interface AdminsDao extends Mapper<Admins> {
 
     @Select("select * from admins where account=${account} and apwd=${apwd}")
-    List<Account> queryName(String account, String apwd);
+    List<Admins> queryName(@Param("account") String account,@Param("apwd") String apwd);
+   @Select("select * from admins where account=${account}")
+   List<Admins> queryaccount(@Param("account") String account);
+////
+//    SELECT * FROM authority
+//    WHERE auth_id in
+//            (SELECT pid FROM role
+//                    WHERE pid=2) and auth_pid=0;
 }
