b957281303c42cd5d906885372625b4e0cd78915
e <1666497703@qq.com>
2020-08-28 08:35:53 +0800

    des更新

--- src/main/java/com/dao/RommtypeDao.java
+++ src/main/java/com/dao/RommtypeDao.java
@@ -3,9 +3,6 @@ package com.dao;
 import com.entity.Rommtype;
 import tk.mybatis.mapper.common.Mapper;
 
-
 @org.apache.ibatis.annotations.Mapper
 public interface RommtypeDao extends Mapper<Rommtype> {
-
-
 }
